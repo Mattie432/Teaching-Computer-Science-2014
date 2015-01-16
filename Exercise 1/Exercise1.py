4752adf427bd2b8097d83341a711ca21af65ee53
Mattie432 <mattie432@icloud.com>
2015-01-16 20:23:34 +0000

    Preamble complete.

--- Exercise 1/Exercise1.py
+++ Exercise 1/Exercise1.py
@@ -1,6 +1,13 @@
+def askForName():
+    #Initial request for the users name.
+    str = raw_input("Please enter your name: ")
+    print "\n","Hello",str,"there will be 10 questions to follow for you to answer."
+    #Pause until the user is ready.
+    raw_input("Are you ready to start? : ")
+
 #Main method to start the program.
 def main():
-    # my code here
+    askForName()
 
 #Used so that if imported as a module main is not instantly called.
 if __name__ == "__main__":
