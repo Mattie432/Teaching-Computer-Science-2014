df0b17dfcc3258f09dcf393d8515b9fb45318a10
Mattie432 <mattie432@icloud.com>
2015-01-17 00:28:30 +0000

    Exercise 1 completed

--- Exercise 1/Exercise1.py
+++ Exercise 1/Exercise1.py
@@ -1,13 +1,83 @@
+#Imports
+from random import randint
+from operator import add, sub, mul
+import random
+
+#Globals
+numOfQuestions = 10
+
+def generateQuestion(questionNumber):
+    #generate two random numbers between min & max
+    minNum = 1
+    maxNum = 12
+    randNum1 = randint(minNum,maxNum)
+    randNum2 = randint(minNum,maxNum)
+
+    #Pick a random operator
+    ops = (add, sub, mul)
+    op = random.choice(ops)
+
+    #Convert the operator to a string for use when printing.
+    if op == add:
+        opStr = "+"
+    elif op == sub:
+        opStr = "-"
+    elif op == mul:
+        opStr = "*"
+
+    #Calculate the answer.
+    ans = op(randNum1, randNum2)
+
+    #Print the questions
+    print "Question [",questionNumber,"]"
+    print randNum1,opStr,randNum2
+
+    #Ask the use for the answer.
+    ansUsr = ""
+    while not ansUsr.isdigit() and (not (ansUsr.startswith('-') and ansUsr[1:].isdigit())) :
+        ansUsr = raw_input("Please enter your Answer: ")
+    #Change input into a number
+    ansUsr = int(ansUsr.strip())
+
+    #Check the answer
+    correctAnswer = ans == ansUsr
+
+    #Print if the answer was correct
+    if correctAnswer:
+        print "Correct! \n"
+    else:
+        print "Incorrect, answer was",ans,"\n"
+
+    #Returns true if the answer was correct.
+    return correctAnswer
+
 def askForName():
+    #Get global variable
+    global numOfQuestions
+
     #Initial request for the users name.
     str = raw_input("Please enter your name: ")
-    print "\n","Hello",str,"there will be 10 questions to follow for you to answer."
-    #Pause until the user is ready.
-    raw_input("Are you ready to start? : ")
 
-#Main method to start the program.
+    #Pause until the user is ready.
+    print "\n","Hello",str,"there will be",numOfQuestions, "questions to follow for you to answer."
+    raw_input("Are you ready to start? : ")
+    print "\n___________________________________________________________________\n"
+
 def main():
+    #Variable to count the correct answers
+    count = 0
+
+    #Preamble to start the progran
     askForName()
+
+    #Ask the number of questions.
+    for i in range(1,numOfQuestions + 1):
+        #If the answer was correct increase the count variable
+        if generateQuestion(i):
+            count = count + 1
+
+    #Print the number of correct answers
+    print "\nYou got",count,"/",numOfQuestions,"correct!\n"
 
 #Used so that if imported as a module main is not instantly called.
 if __name__ == "__main__":
--- Exercise 1/Readme.md
+++ Exercise 1/Readme.md
@@ -2,3 +2,4 @@
 A primary school teacher wants a computer program to test the basic arithmetic skills of her students. The program should generate a quiz consisting of random series of questions, using in each case any two numbers and addition, subtraction and multiplication. the system should ask the student's name, then ask 10 questions, output if the answer to each question is correct or not and produce a final score out of 10.
 
 ## Solution
+Very simple solution. One function to ask a question which can then be used multiple times. There is a loop which calls the `generateQuestion` function the required number of times.
